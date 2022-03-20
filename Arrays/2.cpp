a3331279007524da45db968512b484f703a377f8
palakyadav1807 <palakyadav187@gmail.com>
2022-03-20 13:27:14 +0530

    add solutions of 1,2,3,4,7 and 8

---
+++ Arrays/2.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int arr[], int n){
+        vector<int> res;
+        int curr_max=arr[n-1];
+        res.push_back(arr[n-1]);
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>=curr_max){
+                res.push_back(arr[i]);
+                curr_max=arr[i];
+            }
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+  // } Driver Code Ends
